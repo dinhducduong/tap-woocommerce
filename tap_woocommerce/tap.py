f22e0b763de643d35249d62d8989f428ac149750
Josué M. Sehnem <josue@sehnem.com>
2021-11-18 17:53:15 -0300

    bump version

--- tap_woocommerce/tap.py
+++ tap_woocommerce/tap.py
@@ -30,6 +30,3 @@ class TapWooCommerce(Tap):
     def discover_streams(self) -> List[Stream]:
         """Return a list of discovered streams."""
         return [stream_class(tap=self) for stream_class in STREAM_TYPES]
-
-if __name__=="__main__":
-    TapWooCommerce.cli()
