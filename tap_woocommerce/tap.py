41f52168217f93faa981eb04c21e087b06ce01ed
Đinh Đức Dương <97863053+dinhducduong@users.noreply.github.com>
2023-06-15 15:55:12 +0700

    add stream attribute

--- tap_woocommerce/tap.py
+++ tap_woocommerce/tap.py
@@ -8,12 +8,14 @@ from singer_sdk import typing as th  # JSON schema typing helpers
 from tap_woocommerce.streams import (
     WooCommerceStream,
     ProductsStream,
-    CategoriesStream
+    CategoriesStream,
+    ProductsAttributeStream
 )
 
 STREAM_TYPES = [
     ProductsStream,
-    CategoriesStream
+    CategoriesStream,
+    ProductsAttributeStream
 ]
 
 
